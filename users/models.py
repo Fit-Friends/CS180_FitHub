54849eb385c69634a5e7eb07b9930b57629d2937
Jacob Korte <pantherj1999@gmail.com>
2024-02-15 22:37:43 -0800

    profile class

--- users/models.py
+++ users/models.py
@@ -15,3 +15,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+#profile class
+class Profile(models.Model):
+     id = models.OneToOneField(User, on_delete=models.CASCADE)
+     username = models.CharField(max_length=100)
+     #friends list maybe?
+     #profile picture portion
+     profile_pic = models.ImageField(upload_to="images/")
+     #username is displayed 
+     def __str__(self):
+         return self.username
+#update profile function maybe
