ff9aa6fdb63e0ff1ee058ece092904ee9d1ca26d
jvarg122 <73116975+jvarg122@users.noreply.github.com>
2024-02-17 01:31:09 -0800

    Update urls.py

--- log/urls.py
+++ log/urls.py
@@ -2,10 +2,10 @@ from django import views
 from django.contrib import admin
 from django.urls import include, path
 
-from log.views import ExerciseGetLogView, ExercisePostLogView
-
+from log.views import ExerciseGetLogView, ExercisePostLogView, ExerciseGetLogByIndexView
 urlpatterns = [
     path('log/<int:user_id>', ExerciseGetLogView.as_view()),
-    path('log/', ExercisePostLogView.as_view())
+    path('log/', ExercisePostLogView.as_view()),
     # path('log',ExerciseLogView.as_view())
+    path('getlog/<int:index>/', ExerciseGetLogByIndexView.as_view(), name='get_log_by_index')
 ]
