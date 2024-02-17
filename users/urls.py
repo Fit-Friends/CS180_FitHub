55e05ccacc1eb09656371d44c3a4449351e9a86f
jvarg122 <73116975+jvarg122@users.noreply.github.com>
2024-02-17 01:24:03 -0800

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from .views import ExerciseGetLogByIndexView #
 urlpatterns = [
     path('register/', include('dj_rest_auth.registration.urls')),
-    path('',include('dj_rest_auth.urls'))
+    path('',include('dj_rest_auth.urls')),
+    path('getlog/<int:index>/', ExerciseGetLogByIndexView.as_view(), name='get_log_by_index'),
 ]
